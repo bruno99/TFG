42f7a3dfc7051e014db4e97fdef243b06e64861a
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2022-02-10 16:09:12 +0100

    Create simple_touch.ino

---
+++ simple_touch.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+// constants won't change
+const int TOUCH_SENSOR_PIN = 7; // Arduino pin connected to touch sensor's pin
+const int SERVO_PIN        = 9; // Arduino pin connected to servo motor's pin
+
+Servo servo; // create servo object to control a servo
+
+// variables will change:
+int angle = 0;         // the current angle of servo motor
+int lastTouchState;    // the previous state of touch sensor
+int currentTouchState; // the current state of touch sensor
+
+void setup() {
+  Serial.begin(9600);               // initialize serial
+  pinMode(TOUCH_SENSOR_PIN, INPUT); // set arduino pin to input mode
+  servo.attach(SERVO_PIN);          // attaches the servo on pin 9 to the servo object
+
+  servo.write(angle);
+  currentTouchState = digitalRead(TOUCH_SENSOR_PIN);
+}
+
+void loop() {
+  lastTouchState    = currentTouchState;             // save the last state
+  currentTouchState = digitalRead(TOUCH_SENSOR_PIN); // read new state
+
+  if(lastTouchState =! currentTouchState ) {
+    // change angle of servo motor
+    if(angle == 0)
+      angle = 180;
+    else
+    if(angle == 180)
+      angle = 0;
+
+    // control servo motor arccoding to the angle
+    servo.write(angle);
+  }
+
+  }
